31831f56afaf35714b398afbd4937492adec3ceb
YungPoves <povesr12@gmail.com>
2025-01-07 11:36:10 +0200

    init

---
+++ build-snake-Desktop-Debug/CMakeFiles/snake.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/snake_autogen.dir/ParseCache.txt"
+  "snake_autogen"
+  "CMakeFiles/snake.dir/main.cpp.o"
+  "CMakeFiles/snake.dir/main.cpp.o.d"
+  "CMakeFiles/snake.dir/mainwindow.cpp.o"
+  "CMakeFiles/snake.dir/mainwindow.cpp.o.d"
+  "CMakeFiles/snake.dir/snake_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/snake.dir/snake_autogen/mocs_compilation.cpp.o.d"
+  "snake"
+  "snake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-snake-Desktop-Debug/CMakeFiles/snake_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake_autogen"
+  "snake_autogen/include/ui_mainwindow.h"
+  "snake_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/snake_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
